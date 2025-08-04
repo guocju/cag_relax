7d6af7272f279d48bbda1897c17014d91d91fe53
xiaofan.huang@p2p-inspur <xiaofan.huang@xjtu.edu.cn>
2025-08-04 12:58:01 +0800

    uint8 fix

--- zlocalTest/p2p_mini_test/only_fpga.py
+++ zlocalTest/p2p_mini_test/only_fpga.py
@@ -76,7 +76,7 @@ mod = TestFPGA
 target = [tvm.cpu(0), tvm.fpga(0)]
 vm = compile(mod, target)
 n=260
-inp_numpy = np.empty((n,), dtype="int32")
+inp_numpy = np.empty((n,), dtype="uint8")
 inp_numpy.fill(2)
 inp = tvm.nd.array(inp_numpy, target[0])
 res = vm["foo"](inp)
